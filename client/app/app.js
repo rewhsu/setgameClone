ad4898fcc23fddbf33b5694dfbce4f4593905877
Andrew Hsu <andrewhsu42@gmail.com>
2017-04-14 04:25:42 -0700

    Clean up entry point, separate App component

--- client/app/app.js
+++ client/app/app.js
@@ -1,10 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-class App extends React.Component {
-  render() {
-    return <p>Set Game Goes Here</p>;
-  }
-}
+import App from './components/App';
 
 ReactDOM.render(<App/>, document.getElementById('app'));
---
+++ client/app/components/App.js
@@ -0,0 +1,7 @@
+var React = require('react');
+
+export default class App extends React.Component {
+  render() {
+    return <p>Set Game Goes Here</p>;
+  }
+}
