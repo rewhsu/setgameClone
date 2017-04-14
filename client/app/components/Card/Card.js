2e3765ec0d6faf72611fe1f1575a4e798e082e0b
Andrew Hsu <andrewhsu42@gmail.com>
2017-04-14 07:36:51 -0700

    Convert card to functional stateless component

--- client/app/components/Card/Card.js
+++ client/app/components/Card/Card.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import styles from './Card.css';
 
-export default class Card extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className={styles.cell}>
-        {this.props.id}
-      </div>
-    );
-  }
-}
+var Card = (props) => (
+  <div className={styles.cell}>
+    {props.id}
+  </div>
+)
+
+export default Card;
